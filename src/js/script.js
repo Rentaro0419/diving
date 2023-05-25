b2dee36baf29dcbd3c471faac866360af4ade0d4
{yayoi0814} <{koikoi0814@gmial.com}>
2023-05-25 18:25:25 +0900

    TOPへ戻るの追加・修正ファイルです。

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,11 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
+    ('._c-head').click(function(){
+        $('body,html').animate({
+        scrollTop: 0},500);
+        return false;
 
+})
 });
+
